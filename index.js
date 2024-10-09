9679d2552e7b6e446b7c45206108fc6454597240
Cynthia <kalechecynthia@gmail.com>
2024-10-09 14:42:12 +0300

    works on lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+const employee = {
+  name: "Mark",
+  streetAddress: "Jacaranda 123",
+};
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+  let newObject = {
+    ...employeeObject,
+    [key]: value,
+  };
+  console.log(newObject);
+  return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(
+  employeeObject,
+  key,
+  value
+) {
+  employeeObject[key] = value;
+  console.log(employeeObject);
+  return employeeObject;
+}
+
+function deleteFromEmployeeByKey(employeeObject, key) {
+  let newEmployee = { ...employeeObject };
+  delete newEmployee[key];
+  console.log(newEmployee);
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
+  delete employeeObject[key];
+  console.log(employeeObject);
+  return employeeObject;
+}
+destructivelyDeleteFromEmployeeByKey(employee, "name");
+  
